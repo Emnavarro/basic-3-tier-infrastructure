3fe7a8dbc73205314f8cef8990a8f085ac5ef4a0
Emanuel Navarro <navarrof.emanuel96@gmail.com>
2022-10-10 17:49:30 -0400

    Variables

---
+++ vpc-outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "THe CIDR block of the VPC"
+  value       = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
+
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS nat gateway"
+  value       = module.vpc.nat_public_ips
+}
+
+output "azs" {
+  description = "A list of availability zones specified as arguments"
+  value       = module.vpc.azs
+}
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,6 @@
 data "aws_availability_zones" "available" {
-  state = "available"
-  exclude_names = [ "us-east-1c" ]
-}
-
-variable "vpc_availability_zones" {
-  description = "VPC Availability Zones"
-  type        = list(string)
-  default     = ["us-east-1a","us-east-1b"]  
+  state         = "available"
+  exclude_names = ["us-east-1c"]
 }
 
 #Create VPC Terraform modules
@@ -16,24 +10,26 @@ module "vpc" {
   #version = "~> 3.0"
 
   #VPC Basic Details
-  name = "vpc-dev"
-  cidr = "10.0.0.0/16"
+  name = var.vpc_name
+  cidr = var.vpc_cidr_block
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24","10.0.102.0/24"]
+  azs             = var.vpc_availability_zones
+  public_subnets  = var.vpc_public_subnets
+  private_subnets = var.vpc_private_subnets
+
 
   #Database Subnets
-  create_database_subnet_group = true 
-  create_database_subnet_route_table = true
-  database_subnets = ["10.0.151.0/24","10.0.152.0/24"]
+  database_subnets                   = var.vpc_database_subnets
+  create_database_subnet_group       = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+
 
   #NAT Gateway - Outbound Communication
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = var.vpc_enable_nat_gateway #true
+  single_nat_gateway = var.vpc_single_nat_gateway #true
 
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
 
   public_subnet_tags = {
@@ -46,8 +42,8 @@ module "vpc" {
     Name = "database-subnets"
   }
   tags = {
-    Owner = "Emanuel"
-    Environment = "devss"
+    Owner       = "Emanuel"
+    Environment = "dev"
   }
 }
 
